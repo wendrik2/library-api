f6a38b4ffb248fb5535ca52d61d99f07a53ce30c
wendrik2 <hiler7499@gmail.com>
2026-06-10 20:20:11 +0200

    feat: add BookRepository

---
+++ src/main/java/com/library/library_api/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.library_api.repository;
+
+import com.library.library_api.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
